b14fd5dab8f2d91e18fcae8ad2e4eea24b1038f0
Sébastien L'haire <sebastien@lhaire.org>
2022-02-23 18:49:59 +0100

    laravel 9 compat

---
+++ config/autocompleter.php
@@ -0,0 +1,26 @@
+<?php
+return [
+  'labelclass' => '',
+  'helpclass' => 'autocompletehelp',
+  'helptext' => '#autocompleter::messages.autocompleterhelp#',
+  'divclass' => "form-group",
+  'divinsidestyle' => 'width: 350px',
+  'resultdivclass' => 'sebautocomplete',
+  'resdivstyle' => [
+    'width' => '350px',
+    'top' => '-24px'
+  ],
+  'inputclass' => 'form-control',
+  'highliteclasses' => 'sebautoloadhighlite',
+  'resultlistclass' => 'list-group',
+  'resultlistclassitem' => 'list-group-item',
+  'activeitem' => 'active',
+  'id_field' => 'id',
+  'id_included' => true,
+  'list_field' => 'listfield',
+  'encoding' => 'utf8',
+  'csrfrefreshroute' => null,
+  'maxresults' => 6,
+  'minsearchstr' => 1,
+  'callback' => null
+];
---
+++ src/AutocompleterService.php
@@ -0,0 +1,10 @@
+<?php
+namespace Seblhaire\Autocompleter;
+
+class AutocompleterService implements AutocompleterServiceContract{
+
+
+  public function init($inputid, $labeltext, $url, $options = []){
+    return new Autocompleter($inputid, $labeltext, $url, $options);
+  }
+}
